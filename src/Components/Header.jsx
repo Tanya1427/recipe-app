74edccf9240bd0d3e7cab69853ebc5a6e4576ee7
Tanya1427 <mangundhlatanyaradzwa@gmail.com>
2025-06-05 10:35:43 -0700

    Initial commit: AI recipe generation app

---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import chefClaudeLogo from "../assets/chef-claude-icon.png"
+
+export default function Header(){
+    return (
+    <header>
+        <img src={chefClaudeLogo} alt = "chef claude app"/>
+        <h1>CHEF AI</h1>
+    </header>
+    )
+
+}
---
+++ src/Components/Main.jsx
@@ -0,0 +1,45 @@
+import {useState} from "react"
+import IngredientsList from "./IngredientsList"
+import {getRecipeFromMistral } from "../ai.js"
+import React from "react"
+import HuggingRecipe from "./HuggingRecipe"
+
+export default function Main(){
+
+    const [ingredients, setIngredients] = useState(
+        [])
+    
+    const [recipe, setRecipe] = React.useState("")
+
+    async function getRecipe() {
+        const recipeMarkdown = await getRecipeFromMistral(ingredients)
+        setRecipe(recipeMarkdown)
+    }
+
+    function addIngredient(formData) {
+        const newIngredient = formData.get("ingredient")
+        setIngredients(prevIngredients => [...prevIngredients, newIngredient])
+        }
+
+    return(
+        <main>
+            
+            <form action={addIngredient} className="add-ingredient-form">
+                <input
+                    type="text"
+                    placeholder="e.g. oregano"
+                    aria-label="Add ingredient"
+                    name="ingredient"
+                />
+                <button>Add ingredient</button>
+            </form>
+
+            {ingredients.length > 0 &&
+                <IngredientsList ingredients={ingredients} getRecipe={getRecipe}
+                />
+            }
+            
+            {recipe && <HuggingRecipe recipe={recipe} />}
+        </main>
+    )
+}
